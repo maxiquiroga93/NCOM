0314206ae13bee8ed71fc74a979ec39d58ecbebe
Maxi Quiroga <maxi.quiroga93@gmail.com>
2019-04-22 20:29:34 -0300

    Agrego el scanner como variable estática

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,12 @@ import java.util.Scanner;
 
 public class Main {
 
+	private static Scanner scanner;
+
 	public static void main(String[] args) {
 	
 		System.out.println("Ingresar numero complejo");
-		Scanner scanner = new Scanner(System.in);
+		scanner = new Scanner(System.in);
 		String numero = scanner.nextLine();
 		Parser parser = new Parser();
 		Complejo complejo = parser.parsear(numero);
